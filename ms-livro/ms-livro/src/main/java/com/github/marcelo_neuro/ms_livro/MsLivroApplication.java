ff4e5c468d884964ce451f972852473d5c3368ce
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-19 10:33:23 -0300

    Feat: ms-livro

---
+++ ms-livro/ms-livro/src/main/java/com/github/marcelo_neuro/ms_livro/MsLivroApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelo_neuro.ms_livro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLivroApplication.class, args);
+	}
+
+}
